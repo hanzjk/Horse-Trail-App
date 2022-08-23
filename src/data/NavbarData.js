50dffa5d73b07f99d17a103d7af2cfbdb7fffa21
hanzjk <hansijk@yahoo.com>
2022-08-23 22:34:41 +0530

    Added Header & Footer

---
+++ src/data/NavbarData.js
@@ -0,0 +1,30 @@
+export const data = [
+  {
+    to: "/about",
+    text: "About",
+  },
+  {
+    to: "/trails",
+    text: "Trails",
+  },
+  {
+    to: "/camping",
+    text: "Camping",
+  },
+  {
+    to: "/search",
+    text: "Search",
+  },
+  {
+    to: "/reviews",
+    text: "Reviews",
+  },
+  {
+    to: "/sign-in",
+    text: "Sign In",
+  },
+  {
+    to: "/sign-up",
+    text: "Sign Up",
+  },
+];
---
+++ src/pages/About.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+import {Body } from "../globalStyles";
+
+
+
+function About() {
+  return (
+    <>
+      {/* Add Body tag before each component */}
+      <Body>
+        <p>about</p>
+      </Body>
+    </>
+  );
+}
+
+export default About
+
