789849dbda489f7828ad86db101875786c6b1bef
hanzjk <hansijk@yahoo.com>
2022-08-24 22:50:25 +0530

    Changes

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -29,36 +29,6 @@ function Footer() {
               69 Street, Delta Building, US Road, Los Angeles 123
             </FooterAddress>
 
-<<<<<<< HEAD
-            <Row align="center" margin="auto  0 0 0" gap="1rem">
-              {footerSocialData.map((social, index) => (
-                <FooterSocialIcon
-                  key={index}
-                  href="/"
-                  target="_blank"
-                  aria-label={social.name}
-                >
-                  {social.icon}
-                </FooterSocialIcon>
-              ))}
-            </Row>
-          </FooterColumn>
-          {footerData.map((footerItem, index) => (
-            <FooterLinkItems key={index}>
-              <FooterLinkTitle>{footerItem.title}</FooterLinkTitle>
-              {footerItem.links.map((link, linkIndex) => (
-                <FooterLink key={linkIndex} to="/">
-                  {link}
-                </FooterLink>
-              ))}
-            </FooterLinkItems>
-          ))}
-        </FooterGrid>
-        <FooterRights>Delta © 2021</FooterRights>
-      </FooterWrapper>
-    </Section>
-  );
-=======
 						<RowField>
 							{footerSocialData.map((social, index) => (
 								<FooterSocialIcon
@@ -87,7 +57,6 @@ function Footer() {
 			</FooterWrapper>
 		</Section>
 	);
->>>>>>> d6b31c6 (Search Page Added)
 }
 
 export default Footer;
